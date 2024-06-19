8f6397245b5e04b6b73c2ed0f8dea246af284309
LucassSantoss <lucascross.pereirasantos@gmail.com>
2024-06-19 17:55:34 -0300

    last changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // Altere para o endereço do seu servidor
+  baseURL: 'http://localhost:8000',
 });
 
 export const login = async (email, senha) => {
